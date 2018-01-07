39cb207667c3cbf54e73a8f1100cf614aa346065
iamkevingreen <kevin@thecouch.nyc>
2018-01-07 13:33:30 -0500

    drop in some quotes

--- pages/index.js
+++ pages/index.js
@@ -19,8 +19,7 @@ export default class extends Component {
     super(props)
 
     this.state = {
-      plantOne: false,
-      plantTwo: false
+      activeQuote: 3
     }
   }
   componentDidMount () {
@@ -73,6 +72,9 @@ export default class extends Component {
           .projects {
             padding: 20px;
           }
+          .quotes {
+            display: none;
+          }
           @media (min-width: 80em) {
             .period {
               max-width: 22px;
@@ -86,6 +88,59 @@ export default class extends Component {
               margin-left: 140px;
             }
           }
+          @media (min-width: 55rem) and (min-height: 800px) {
+            .projects {
+              margin-left: 160px;
+              margin-right: 340px;
+            }
+            .about {
+              position: fixed;
+              margin-left: 0px;
+              bottom: 0;
+              max-height: 400px;
+              width: 300px;
+              right: 0;
+              display: flex;
+              flex-direction: column;
+              justify-content: space-between;
+              align-items: bottom;
+              flex-wrap: wrap;
+              border-top: 5px solid black;
+              border-left: 5px solid black;
+              background: white;
+              z-index: 5;
+              border-right: 5px solid black;
+            }
+            .about > div {
+              width: 100%;
+            }
+            .quotes {
+              position: fixed;
+              right: 0;
+              width: 300px;
+              top: 0;
+              height: calc(100vh - 240px);
+              border-left: 5px solid black;
+              display: flex;
+              flex-wrap: wrap;
+              align-items: center;
+            }
+            .quotes__single {
+              border-bottom: 5px solid black;
+              padding: 14px;
+              height: 2px;
+              text-align: center;
+              cursor: pointer;
+              overflow: hidden;
+            }
+            .quotes__single p {
+              font-weight: 600;
+              font-size: 28px;
+            }
+            .quotes__single.active {
+              height: auto;
+            }
+          }
           `}</style>
         <div>
           <div className='wrapper main'>
@@ -97,6 +152,13 @@ export default class extends Component {
               <div className='about__link'>
                 <a className='cw caps' href="mailto:kevin@thecouch.nyc">Write me a letter</a>
               </div>
+            </div>
+            <div className='quotes'>
+              {API.quotes.map((quote, i) => {
+                return (
+                  <div onMouseOver={() => this.setState({activeQuote: i})} className={`quotes__single ${i === this.state.activeQuote ? 'active' : null}`} key={i}><p>{quote.quote}</p></div>
+                )
+              })}
             </div>
             <div className='projects'>
               {API.data.map((project, i) => (
